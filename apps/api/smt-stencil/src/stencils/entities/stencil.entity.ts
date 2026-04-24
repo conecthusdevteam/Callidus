a7475d9ff688175980a18183b4b66283ec215ae7
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-04-24 09:11:00 -0400

    feat: create stencil entity

---
+++ apps/api/smt-stencil/src/stencils/entities/stencil.entity.ts
@@ -0,0 +1,55 @@
+import { nanoId } from 'nano-id';
+import { BeforeInsert, Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+export enum WashStatus {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+}
+
+@Entity('stencils')
+export class Stencil {
+    @PrimaryColumn()
+    id!: string;
+
+    @Column()
+    stencilCode!: string;
+
+    @Column()
+    manufactureId!: string;
+
+    @Column()
+    country!: string;
+
+    @Column()
+    thickness!: number;
+
+    @Column()
+    addressing!: string;
+
+    @Column()
+    totalWashes!: number;
+
+    @Column()
+    operator!: string;
+
+    @Column()
+    lineName!: string;
+
+    @Column({
+        type: 'simple-enum',
+        enum: WashStatus,
+        default: WashStatus.ACTIVE,
+    })
+    status!: WashStatus;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+    @BeforeInsert()
+    generateId() {
+        this.id = `stencil_${nanoId()}`
+    }
+}
