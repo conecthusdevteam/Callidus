07891859830ee58d72e2403d09cf66f5dfd5f998
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-04-29 10:09:51 -0400

    chore: adjust app.module orm config

--- apps/api/smt-stencil/src/app.module.ts
+++ apps/api/smt-stencil/src/app.module.ts
@@ -17,11 +17,19 @@ import { Plate } from './plates/entities/plate.entity';
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'sqlite',
+        type: 'mssql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        entities: [Stencil, Plate],
+        entities: [__dirname + '/**/*.entity{.ts, .js}'],
         synchronize: configService.get<string>('NODE_ENV') !== 'production',
         logging: configService.get<string>('NODE_ENV') !== 'development',
+        options: {
+          encrypt: configService.get<string>('NODE_ENV') !== 'development',
+          trustServerCertificate: true,
+        }
       })
     }),
     StencilsModule, 
