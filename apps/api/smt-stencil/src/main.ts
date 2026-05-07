c33f732f8558084b040659dd1ea63b35af4e8db5
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-05-07 10:32:49 -0400

    chore: adjust docker files to run on differents environments depending on the context

--- apps/api/smt-stencil/src/main.ts
+++ apps/api/smt-stencil/src/main.ts
@@ -4,9 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const clientPort = process.env.CLIENT_PORT ?? 5173;
   
   app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5172', 'http://10.255.255.170:5172'],
+    origin: [`http://localhost:${clientPort}`, `http://10.255.255.170:${clientPort}`],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
