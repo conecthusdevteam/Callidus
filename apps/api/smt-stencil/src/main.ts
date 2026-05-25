7c632d6c3c9ea51e42778dc28e2b3e1bdaab8d30
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-05-25 10:31:59 -0400

    chore: apply global date interceptor

--- apps/api/smt-stencil/src/main.ts
+++ apps/api/smt-stencil/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DateConverterInterceptor } from './common/interceptors/date-converter.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -35,6 +36,8 @@ async function bootstrap() {
       customSiteTitle: 'LSP API - Documentation',
     });
   }
+
+  app.useGlobalInterceptors(new DateConverterInterceptor());
 
   app.useGlobalPipes(
     new ValidationPipe({
