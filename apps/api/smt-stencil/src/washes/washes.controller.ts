a8f061ba35cbd9ff612e5bbe35c78d90ef555a22
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-05-25 09:40:40 -0400

    chore: swagger docs of controllers

--- apps/api/smt-stencil/src/washes/washes.controller.ts
+++ apps/api/smt-stencil/src/washes/washes.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { WashesService } from './washes.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('washes')
 @Controller('washes')
 export class WashesController {
   constructor(private readonly washesService: WashesService) {}
 
   @Get('daily-summary')
+  @ApiOperation({ summary: 'Retrieve daily summary of washes' })
+  @ApiResponse({ status: 200, description: 'Returns the daily summary of washes.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   getDailySummary() {
     return this.washesService.getDailySummary();
   }
