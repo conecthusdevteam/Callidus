28d5fb0f5ced4d4c5930867a38008437439335dd
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-05-25 09:40:19 -0400

    chore: swagger docs on dto's

--- apps/api/smt-stencil/src/plates/dto/create-plate-wash.dto.ts
+++ apps/api/smt-stencil/src/plates/dto/create-plate-wash.dto.ts
@@ -1,12 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreatePlateWashDto {
+  @ApiProperty({
+    description: 'The ID of the plate to be washed',
+    example: 'plate_SOypWQVWYl1XUGKRyAZyV',
+  })
+  @IsString()
+  plateId!: string;
+
+  @ApiProperty({
+    description: 'The name of the operator performing the wash',
+    example: 'John Doe',
+  })  
   @IsString()
   operator!: string;
 
+  @ApiProperty({
+    description: 'The shift during which the wash is performed',
+    example: 1,
+  })
   @IsNumber()
   shift!: number;
-
+  
+  @ApiProperty({
+    description: 'The phase of the wash process',
+    example: 2,
+  })
   @IsNumber()
   phase!: number;
 }
