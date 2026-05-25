0b1293f6f2107f7f39feb434d850cf42bb018f6a
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-05-25 10:31:30 -0400

    feat: create a date interceptor

---
+++ apps/api/smt-stencil/src/common/interceptors/date-converter.interceptor.ts
@@ -0,0 +1,41 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class DateConverterInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            map(data => {
+                const dateFields = ['created_at', 'updated_at'];
+
+                const convertDates = (obj: any) => {
+                    if (!obj) return;
+
+                    if (Array.isArray(obj)) {
+                        obj.forEach(item => convertDates(item));
+                    } else if (typeof obj === 'object') {
+                        for (const key of dateFields) {
+                            if (obj[key]) {
+                                const date = new Date(obj[key]);
+                                if (!isNaN(date.getTime())) {
+                                    const manausDate = new Date(date.getTime() - 4 * 60 * 60 * 1000);
+
+                                    obj[key] = manausDate.toISOString();
+                                }
+                            }
+                        }
+                        for (const key in obj) {
+                            if (obj[key] && typeof obj[key] === 'object') {
+                                convertDates(obj[key]);
+                            }
+                        }
+                    }
+                };
+
+                convertDates(data);
+                return data;
+            })
+        );
+    }
+}
